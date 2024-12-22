47c8bd3d996c41db3fa8d94054d17ede6e5e1199
Renan Victor <renan22rsw@gmail.com>
2024-12-22 13:01:22 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1>Seja Bem vindo</h1>
+      <Button>Teste</Button>
+    </div>
+  );
+};
+
+export default Home;
