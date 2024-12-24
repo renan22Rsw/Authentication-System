f660a39a951f38582cb152bcf38666096ee7d14b
Renan Victor <renan22rsw@gmail.com>
2024-12-23 21:57:21 -0300

    feat: create user

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -5,6 +5,11 @@
 import * as z from "zod";
 import { registerSchema } from "@/schemas/index";
 
+import prisma from "@/database/index";
+
+import bycrypt from "bcrypt"; //lib para criptografia
+import { getUserByEmail } from "@/data/user";
+
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFields = registerSchema.safeParse(values);
 
@@ -14,5 +19,25 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
     };
   }
 
-  return { success: "email sent!" };
+  const { email, name, password } = validatedFields.data;
+
+  const hashedPassword = await bycrypt.hash(password, 10); //variavel que contem senha criptografada
+
+  const existingEmail = await getUserByEmail(email);
+
+  if (existingEmail) {
+    return {
+      error: "Email already exists",
+    };
+  }
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created" };
 };
---
+++ src/data/user.ts
@@ -0,0 +1,27 @@
+import prisma from "@/database";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  } catch (err) {
+    return { error: err };
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  } catch (err) {
+    return { error: err };
+  }
+};
