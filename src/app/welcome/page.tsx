6b5cc777571a152c6e31ccc6b404f6da32646136
Renan Victor <renan22rsw@gmail.com>
2025-01-12 21:13:28 -0300

    existing user action

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -9,9 +9,11 @@ import Link from "next/link";
 import UserImage from "@/components/user-image";
 
 import noPicture from "../../../public/no-user-picture.jpg";
+import { existingUser } from "@/actions/idUser";
 
 const WelcomePage = async () => {
   const session = await auth();
+  const idUser = await existingUser(session?.user?.id as string);
 
   if (!session) {
     redirect("/auth/login");
@@ -23,9 +25,11 @@ const WelcomePage = async () => {
 
       <main className="flex h-screen flex-col items-center justify-center space-y-4">
         <UserInfo user={session.user?.name as string} />
-        <Link href={"/settings"}>
-          <Button>Settings</Button>
-        </Link>
+        {idUser.sucess ? (
+          <Link href={"/settings"}>
+            <Button>Settings</Button>
+          </Link>
+        ) : null}
         <UserImage
           img={session.user?.image ? (session.user.image as string) : noPicture}
         />
